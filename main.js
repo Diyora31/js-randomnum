fede8eddd5ea81b7ee8cbbe3eb83a1a3078e0ef8
Diyora31 <asilovad7>
2024-04-19 13:17:36 +0500

    done

---
+++ main.js
@@ -0,0 +1,15 @@
+// console.log(Math.random() * 100); 
+// let num = 5.7
+
+
+// let prompt1 = +prompt(number)
+// let prompt2 = +prompt(number)
+// console.log(Math.round(prompt1 + prompt2))
+
+
+let num =(Math.floor(Math.random()*10))
+// console.log( 'random son:num')
+// console.log(num*2)
+// console.log(num/2);
+// alert(num)
+alert(`randomnumber:${num}   r.number*2: ${num*2}   r.number/2:${num/2}`)
